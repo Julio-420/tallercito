42dd2f0cb769a394d19c09b32b61cb1379d366d0
Julio-420 <julianvasquez661@gmail.com>
2023-11-15 08:49:20 -0500

    hola1

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,49 @@ const App = () => {
           <button></button> 
           
           
-            <h1 className="uppercase text-xs font-semibold text-white py-2" >Product</h1> 
-            <button><Flecha1></Flecha1></button>
             
-            <h1 className="uppercase text-xs font-semibold text-white py-2" >Company</h1> 
-            <button><Flecha1></Flecha1></button>
-            <h1 className="uppercase text-xs font-semibold text-white py-2" >Connect</h1> 
-            <button><Flecha1></Flecha1></button>
+              <h3 className="uppercase text-xs font-semibold text-white py-2" >Product <button><Flecha1></Flecha1></button></h3> 
+              
+              
+              <h1 className="uppercase text-xs font-semibold text-white py-2" >Company<button><Flecha1></Flecha1></button></h1> 
+              
+              <h1 className="uppercase text-xs font-semibold text-white py-2" >Connect<button><Flecha1></Flecha1></button></h1> 
+            
+            
             <div className="flex justify-around ml-15 mr-15">
               <form className="uppercase text-sm font-semibold text-white rounded-full border-2 px-4 py-4" >  log in   </form>
               <form className="uppercase text-sm font-semibold text-black rounded-full border-2 px-4 py-4 bg-white" >  sing up  </form>
             </div>
             </div>
             </header>
+            <br />
+            <br />
           
-          <div className="py-12 my-12">
-            <div className="flex justify-center  ">
-              <h3 className="uppercase text-2xl font-semibold text-white " >  A modern publishing platafom </h3>
+        <section>
+            <div className="py-12 my-12">
+              <div className="flex justify-center  ">
+                <h3 className="uppercase text-2xl font-semibold text-white " >  A modern publishing platafom </h3>
+              </div>
+              <div className="flex justify-center ">
+                <h3 className="uppercase text-sm font-semibold text-white " >  grow you audience and buil your online  </h3>
+              </div>
+              <br />
+              <br />
+              <section className="">
+                <div className="flex  justify-center gap-4">
+                  <form className="uppercase text-sm font-semibold text-white rounded-full border-2 px-4 py-4" >  star for free  </form>
+                  <form className="uppercase text-sm font-semibold text-black rounded-full border-2 px-4 py-4 bg-white" >  learn more   </form>
+                </div>
+              </section>
             </div>
-            <div className="flex justify-center ">
-              <h3 className="uppercase text-sm font-semibold text-white " >  grow you audience and buil your online  </h3>
-            </div>
-            <div className="flex justify-around ml-15 mr-15">
-              <form className="uppercase text-sm font-semibold text-white rounded-full border-2 px-4 py-4" >  star for free  </form>
-              <form className="uppercase text-sm font-semibold text-black rounded-full border-2 px-4 py-4 bg-white" >  learn more   </form>
-            </div>
-          </div>
+        </section>
 
           <button></button>
 
         </div>
+        <main>
+          <section className="bg-gray-800 w-full h-screen flex items-end justify-start relative overflow-"></section>
+        </main>
     
       
     
